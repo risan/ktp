2d6b90b7367ed060ab8908cf04c6d6d58ab0200c
risan <risanbagja@gmail.com>
2016-03-03 15:13:38 +0100

    Add test suites for HttpClient.

---
+++ tests/units/HttpClientTest.php
@@ -0,0 +1,23 @@
+<?php
+use Ktp\HttpClient;
+use Psr\Http\Message\ResponseInterface;
+
+class HttpClientTest extends PHPUnit_Framework_TestCase {
+    /** @test */
+    function http_client_has_base_uri()
+    {
+        $httpClient = new HttpClient('http://foo.bar');
+
+        $this->assertEquals('http://foo.bar', $httpClient->baseUri());
+    }
+
+    /** @test */
+    function http_client_has_post_request()
+    {
+        $httpClient = new HttpClient('http://www.mocky.io/v2/');
+
+        $response = $httpClient->post('5678638b0f00006a2a500861');
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+    }
+}
